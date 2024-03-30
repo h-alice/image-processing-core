a091a4e798b06159b427a3a68a3f6c64bd03742e
h-alice <admin@halice.art>
2024-03-31 04:04:38 +0800

    Rewrite ICC embeddeing.

--- operation/image_icc_profile.go
+++ operation/image_icc_profile.go
@@ -1,1 +1,54 @@
 package operation
+
+import (
+	"bytes"
+	icc "imagetools/icc"
+	image_parser "imagetools/image_parser"
+)
+
+func (CurrentProcessingImage) EmbedProfile(profile_name string) Operation {
+
+	return func(currentImage CurrentProcessingImage) (CurrentProcessingImage, error) {
+
+		// Input image should in binary format.
+		if !currentImage.IsBinary() {
+			// Change the error state.
+			currentImage.errorState = ErrOperationNotSupportInImage
+			// Return error.
+			return currentImage, ErrOperationNotSupportInImage
+		}
+
+		// Create reader from binary data.
+		r := bytes.NewReader(currentImage.ImageData)
+
+		// Parse binary image to segments.
+		parsed_image, err := image_parser.Parse(r)
+		if err != nil {
+			// Change the error state.
+			currentImage.errorState = err
+			// Return error.
+			return currentImage, err
+		}
+
+		err = icc.EmbedIccProfile(profile_name, parsed_image)
+		if err != nil {
+			// Change the error state.
+			currentImage.errorState = err
+			// Return error.
+			return currentImage, err
+		}
+
+		// Create a buffer to hold the image data.
+		buf := new(bytes.Buffer)
+		_, err = parsed_image.WriteTo(buf)
+		if err != nil {
+			// Change the error state.
+			currentImage.errorState = err
+			// Return error.
+			return currentImage, err
+		}
+
+		return CurrentProcessingImage{ImageData: buf.Bytes(), isBinaryData: true}, nil
+	}
+
+}
