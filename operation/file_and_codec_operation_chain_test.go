119cf0e4636a273b3c39a4527b27fca10533504b
h-alice <admin@halice.art>
2024-03-30 16:34:14 +0800

    Rewrite test.

--- operation/file_and_codec_operation_chain_test.go
+++ operation/file_and_codec_operation_chain_test.go
@@ -1,9 +1,32 @@
 package operation
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
 
-func TestCretaeImageFromFile(t *testing.T) {
+func TestPreTestTesting(t *testing.T) {
+	test_png_relative_path := "./test_resources/test_ayaya.png"
+	_, err := os.ReadFile(test_png_relative_path)
+	if err != nil {
+		t.Errorf("(Pre-testing) Error reading file: %v", err)
+	}
 
+	test_jpg_relative_path := "./test_resources/test_ayaya.jpg"
+	_, err = os.ReadFile(test_jpg_relative_path)
+	if err != nil {
+		t.Errorf("(Pre-testing) Error reading file: %v", err)
+	}
+}
+
+func TestCreateImageFromFile(t *testing.T) {
+	test_png_relative_path := "./test_resources/test_ayaya.png"
+
+	im, err := CreateImageFromFile(test_png_relative_path)
+	if err != nil {
+		t.Errorf("Error creating image from file: %v", err)
+	}
+
+	fmt.Printf("%#v\n", im)
 }
