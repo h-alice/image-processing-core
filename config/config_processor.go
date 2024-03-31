7352af0fdd3e37eabb4851fa28c6791cdfa23c32
h-alice <admin@halice.art>
2024-03-31 21:11:18 +0800

    completely remove image process in config file processor

--- config/config_processor.go
+++ config/config_processor.go
@@ -3,12 +3,7 @@ package config
 import (
 	"encoding/json"
 	"errors"
-	"image"
-	icc "imagetools/icc"
-	image_parser "imagetools/image_parser"
 	op "imagetools/operation"
-	"io"
-	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -60,68 +55,6 @@ type ProfileConfig struct {
 	ICC         string        // ICC profile to embed
 	Resize      *ResizeConfig // Resize option
 	Output      *OutputConfig // Output file configuraion
-}
-
-func (profile ProfileConfig) DoCrop(in image.Image) (image.Image, error) {
-	return in, nil
-}
-
-// Embed ICC profile to image.
-func (profile ProfileConfig) DoEmbedIcc(out io.Writer, in io.Reader) error {
-
-	parsed_image, err := image_parser.Parse(in)
-	if err != nil {
-		return err
-	}
-
-	err = icc.EmbedIccProfile(profile.ICC, parsed_image)
-	if err != nil {
-		return err
-	}
-
-	_, err = parsed_image.WriteTo(out)
-
-	return err
-}
-
-func (profile ProfileConfig) ProcessFile(out io.Writer, in io.Reader) error {
-
-	// Procedure: Decode -> image ops -> encode -> segment ops -> write out
-
-	working_image, err := op.CreateImageFromReader(in)
-	if err != nil {
-		log.Printf("[x] Error while creating image: %v", err)
-		return err
-	}
-
-	// Do crop
-	//img, err = profile.DoCrop(img)
-	//if err != nil {
-	//	log.Printf("[x] Error while cropping image: %v", err)
-	//	return err
-	//}
-
-	output_image := working_image.
-		Then(op.Decode()).
-		ThenIf(profile.Resize.Factor != 0.0, op.ResizeImageByFactor(profile.Resize.Algorithm, profile.Resize.Factor)).
-		ThenIf((profile.Resize.Factor == 0.0) && (profile.Resize.Width != 0), op.ResizeImageByWidth(profile.Resize.Algorithm, profile.Resize.Width)).
-		ThenIf((profile.Resize.Factor == 0.0) && (profile.Resize.Width == 0) && (profile.Resize.Height != 0), op.ResizeImageByHeight(profile.Resize.Algorithm, profile.Resize.Height)).
-		Then(op.Encode(profile.Output.Format, (*op.EncoderOption)(profile.Output.Options))).
-		ThenIf((profile.Output.Format == "jpeg" || profile.Output.Format == "jpg") && profile.ICC != "", op.EmbedProfile(profile.ICC)) // Supports jpeg only for now, will be extended to other formats.
-
-	if output_image.LastError() != nil {
-		log.Printf("[x] Error while processing image: %v", output_image.LastError())
-		return output_image.LastError()
-	}
-
-	// Write output to writer.
-	output_image.Then(op.WriteImageToWriter(out))
-	if output_image.LastError() != nil {
-		log.Printf("[x] Error while writing image: %v", output_image.LastError())
-		return output_image.LastError()
-	}
-
-	return nil
 }
 
 type OutputDirConfig struct {
