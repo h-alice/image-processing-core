d375010c17a8994242fb4fb835ff0cecbbad5ff0
h-alice <admin@halice.art>
2024-04-02 22:53:10 +0800

    Test file documentation.

--- operation/image_cropping_test.go
+++ operation/image_cropping_test.go
@@ -16,7 +16,7 @@ func TestImageCropping(t *testing.T) {
 	img = img.Then(Decode())
 
 	// Crop image.
-	crop_area := image.Rect(5, 10, 20, 24)
+	crop_area := image.Rect(7, 13, 20, 24)
 	t.Logf("Cropping area: %v", crop_area)
 
 	cropped_img, err := cropImageInternal(img.Image, crop_area)
@@ -34,20 +34,20 @@ func TestImageCropping(t *testing.T) {
 	// Compare images, pixel-by-pixel.
 	for x := 0; x < cropped_img.Bounds().Dx(); x++ {
 		for y := 0; y < cropped_img.Bounds().Dy(); y++ {
-			crop_start_x := crop_area.Min.X
-			crop_start_y := crop_area.Min.Y
+
+			// Calculate the starting point of cropping area.
+			crop_start := image.Point{X: crop_area.Min.X, Y: crop_area.Min.Y}
+
+			// Calculate the current evaluating point on the original image by shifting the starting point of cropping area.
+			current_point_original_image := crop_start.Add(image.Point{X: x, Y: y})
+
 			// NOTE: This is a tricky part. If using At(x, y) to compare the pixel, it may not work as expected.
 			//       So, we use RGBA64At(x, y) instead.
-			if img.Image.Bounds().RGBA64At(crop_start_x+x, crop_start_y+y) != cropped_img.Bounds().RGBA64At(x, y) {
+			if img.Image.Bounds().RGBA64At(current_point_original_image.X, current_point_original_image.Y) != cropped_img.Bounds().RGBA64At(x, y) {
 				t.Logf("Image 1: %v", img.Image.At(x, y))
 				t.Logf("Image 2: %v", cropped_img.At(x, y))
 				t.Fatalf("Images are not equal at (%d, %d)", x, y)
 			}
 		}
 	}
-
-	img.Image = cropped_img
-
-	// Save image.
-	img.Then(Encode("png", nil)).Then(WriteImageToFile("test_resources/test_ayaya_cropped.png"))
 }
