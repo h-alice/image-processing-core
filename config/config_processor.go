d998710580383b11b8150f35454a48390cf2bb50
h-alice <admin@halice.art>
2024-04-03 00:28:55 +0800

    Refactoring config file definition.

--- config/config_processor.go
+++ config/config_processor.go
@@ -12,13 +12,69 @@ import (
 
 var ErrNotImplemented = errors.New("operation not implemented")
 
+// OutputOptionConfig structure for output file.
 type OutputOptionConfig op.EncoderOption
 
+// Config structure for output file.
+//
+// Format: Output file format. Either `jpeg` or `png`.
+//
+// NameSuffix: Suffix of the output file name.
+//
+// NamePrefix: Prefix of the output file name.
+//
+// Options: Encoder option. For jpeg use and supports only `Quality` option.
 type OutputConfig struct {
 	Format     string              `yaml:"format"`     // Output file format
 	NameSuffix string              `yaml:"nameSuffix"` // Output file name suffix
 	NamePrefix string              `yaml:"namePrefix"` // Output file name prefix
 	Options    *OutputOptionConfig `yaml:"options"`    // Encoder option
+}
+
+// Config structure for resizing image.
+//
+// Width: Output image width.
+//
+// Height: Output image height.
+//
+// Factor: Resize factor.
+//
+// Algorithm: Resize algorithm. Either `nearestneighbor`, `catmullrom`, or `approxbilinear`.
+//
+// NOTE: The `Factor` is prioritized over `Width` and `Height`.
+type ResizeConfig struct {
+	Width     int     `yaml:"width"`     // Output image width
+	Height    int     `yaml:"height"`    // Output image height
+	Factor    float32 `yaml:"factor"`    // Resize factor
+	Algorithm string  `yaml:"algorithm"` // Resize algorithm
+}
+
+// Config structure for processing profile.
+//
+// ProfileName: Profile identifier.
+//
+// ICC: ICC profile to embed.
+//
+// Resize: Resize option.
+//
+// Output: Output file configuration.
+type ProcessProfileConfig struct {
+	ProfileName string        `yaml:"profileName"` // Profile identifier
+	ICC         string        `yaml:"icc"`         // ICC profile to embed
+	Resize      *ResizeConfig `yaml:"resize"`      // Resize option
+	Output      *OutputConfig `yaml:"output"`      // Output file configuration
+}
+
+// Currently not used.
+type OutputDirConfig struct {
+	DirName *string `yaml:"dirName"` // Output directory name
+}
+
+// Config structure for config file.
+//
+// Profiles: List of profile configurations.
+type ConfigFileRoot struct {
+	Profiles []ProcessProfileConfig `yaml:"profiles"` // List of profile configurations
 }
 
 // Generate output file name.
@@ -44,30 +100,8 @@ func (ocf OutputConfig) GenerateFileName(input_name string) string {
 	return full_file
 }
 
-type ResizeConfig struct {
-	Width     int     `yaml:"width"`     // Output image width
-	Height    int     `yaml:"height"`    // Output image height
-	Factor    float32 `yaml:"factor"`    // Resize factor
-	Algorithm string  `yaml:"algorithm"` // Resize algorithm
-}
-
-type ProcessProfileConfig struct {
-	ProfileName string        `yaml:"profileName"` // Profile identifier
-	ICC         string        `yaml:"icc"`         // ICC profile to embed
-	Resize      *ResizeConfig `yaml:"resize"`      // Resize option
-	Output      *OutputConfig `yaml:"output"`      // Output file configuration
-}
-
-type OutputDirConfig struct {
-	DirName *string `yaml:"dirName"` // Output directory name
-}
-
-type ConfigFile struct {
-	Profiles []ProcessProfileConfig `yaml:"profiles"` // List of profile configurations
-}
-
 // Load config file from path.
-func LoadConfig(config_path string) (*ConfigFile, error) {
+func LoadConfigFromFile(config_path string) (*ConfigFileRoot, error) {
 
 	raw_config, err := os.ReadFile(config_path) // Read raw config file.
 	if err != nil {
@@ -75,7 +109,7 @@ func LoadConfig(config_path string) (*ConfigFile, error) {
 	}
 
 	// Converting JSON to config structure.
-	var conf ConfigFile                     // Parsed config placeholder.
+	var conf ConfigFileRoot                 // Parsed config placeholder.
 	err = yaml.Unmarshal(raw_config, &conf) // Convert JSON to structure.
 	if err != nil {
 		return nil, err
