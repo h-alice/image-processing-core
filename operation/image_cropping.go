275c92ddf1b33c41adb70d2166c8b898f581fa62
h-alice <admin@halice.art>
2024-04-02 23:07:22 +0800

    Center alignment function.

--- operation/image_cropping.go
+++ operation/image_cropping.go
@@ -14,7 +14,32 @@ var (
 )
 
 // Defines a prototype for crop alignment function. (e.g. Center, TopLeft, BottomRight)
-type cropAlignment func(image.Rectangle, int, int) image.Rectangle
+// The first parameter is the boundary of the original image.
+// The second and third parameters are the width and height of the cropping area, respectively.
+type CropAlignment func(image.Rectangle, int, int) image.Rectangle
+
+func (CropAlignment) CenterAlignment(original_image_boundary image.Rectangle, cropped_width, cropped_height int) image.Rectangle {
+
+	// Calculate the center point of the original image.
+	center := image.Point{
+		X: original_image_boundary.Dx() / 2,
+		Y: original_image_boundary.Dy() / 2,
+	}
+
+	// Calculate the starting point of the cropping area.
+	crop_start := image.Point{
+		X: center.X - (cropped_width / 2),
+		Y: center.Y - (cropped_height / 2),
+	}
+
+	// Calculate the ending point of the cropping area.
+	crop_end := image.Point{
+		X: crop_start.X + cropped_width,
+		Y: crop_start.Y + cropped_height,
+	}
+
+	return image.Rect(crop_start.X, crop_start.Y, crop_end.X, crop_end.Y)
+}
 
 // Crop image by specifying the boundary.
 func cropImageInternal(input_img image.Image, crop_boundary image.Rectangle) (image.Image, error) {
