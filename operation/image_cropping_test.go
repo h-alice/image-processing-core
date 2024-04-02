9dba267d2a0968bea1ccfad055bc34b1816408a7
h-alice <admin@halice.art>
2024-04-02 23:28:57 +0800

    Test case for top-left alignment.

--- operation/image_cropping_test.go
+++ operation/image_cropping_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestImageCropping(t *testing.T) {
+func TestImageCroppingInternal(t *testing.T) {
 
 	// Load image.
 	img, err := CreateImageFromFile("test_resources/test_ayaya.png")
@@ -50,4 +50,52 @@ func TestImageCropping(t *testing.T) {
 			}
 		}
 	}
+}
+
+func TestImageCroppingTopLeft(t *testing.T) {
+
+	// Load image.
+	img, err := CreateImageFromFile("test_resources/test_ayaya.png")
+	if err != nil {
+		t.Fatalf("Failed to load image: %v", err)
+	}
+
+	img = img.Then(Decode())
+
+	cropped_img := img.Then(Crop(12, 13, TopLeftAlignment))
+
+	if cropped_img.LastError() != nil {
+		t.Fatalf("Failed to crop image: %v", cropped_img.LastError())
+	}
+
+	// Calculate the cropping area.
+	crop_area := TopLeftAlignment(img.Image.Bounds(), 12, 13)
+
+	// Check if cropped image bounds are correct.
+	if crop_area.Min.X != 0 || crop_area.Min.Y != 0 || crop_area.Max.X != 12 || crop_area.Max.Y != 13 {
+		t.Fatalf("Cropping area is not correct: %v", crop_area)
+	} else {
+		t.Logf("Cropping area: %v", crop_area)
+	}
+
+	// Compare images, pixel-by-pixel.
+	for x := 0; x < cropped_img.Image.Bounds().Dx(); x++ {
+		for y := 0; y < cropped_img.Image.Bounds().Dy(); y++ {
+
+			// Calculate the starting point of cropping area.
+			crop_start := image.Point{X: crop_area.Min.X, Y: crop_area.Min.Y}
+
+			// Calculate the current evaluating point on the original image by shifting the starting point of cropping area.
+			current_point_original_image := crop_start.Add(image.Point{X: x, Y: y})
+
+			// NOTE: This is a tricky part. If using At(x, y) to compare the pixel, it may not work as expected.
+			//       So, we use RGBA64At(x, y) instead.
+			if img.Image.Bounds().RGBA64At(current_point_original_image.X, current_point_original_image.Y) != cropped_img.Image.Bounds().RGBA64At(x, y) {
+				t.Logf("Image 1: %v", img.Image.At(x, y))
+				t.Logf("Image 2: %v", cropped_img.Image.At(x, y))
+				t.Fatalf("Images are not equal at (%d, %d)", x, y)
+			}
+		}
+	}
+
 }
