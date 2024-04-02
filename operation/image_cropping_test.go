342395059168f32df7796da53fef5e4a73f90dc9
h-alice <admin@halice.art>
2024-04-02 22:13:25 +0800

    Image cropping test.

--- operation/image_cropping_test.go
+++ operation/image_cropping_test.go
@@ -20,21 +20,22 @@ func TestImageCropping(t *testing.T) {
 		t.Fatalf("Failed to crop image: %v", err)
 	}
 
-	// Set cropped image to current image.
-	img.Image = cropped_img
+	// Compare images.
+	if cropped_img.Bounds().Dx() != 14 || cropped_img.Bounds().Dy() != 14 {
+		t.Fatalf("Cropped image bounds are not correct: %v", cropped_img.Bounds())
+	} else {
 
-	// Encode image.
-	img = img.Then(Encode("jpeg", nil))
-
-	// Write image to file.
-	err = img.Then(WriteImageToFile("test_resources/test1_cropped.jpg")).LastError()
-	if err != nil {
-		t.Fatalf("Failed to write image: %v", err)
+		t.Logf("Image bounds: %v", cropped_img.Bounds())
 	}
-
-	// Remove file.
-	//err = os.Remove("test_resources/test1_cropped.jpg")
-	//if err != nil {
-	//	t.Fatalf("Failed to remove file: %v", err)
-	//}
+	for x := 0; x < cropped_img.Bounds().Dx(); x++ {
+		for y := 0; y < cropped_img.Bounds().Dy(); y++ {
+			// NOTE: This is a tricky part. If using At(x, y) to compare the pixel, it may not work as expected.
+			//       So, we use RGBA64At(x, y) instead.
+			if img.Image.Bounds().RGBA64At(x, y) != cropped_img.Bounds().RGBA64At(x, y) {
+				t.Logf("Image 1: %v", img.Image.At(x, y))
+				t.Logf("Image 2: %v", cropped_img.At(x, y))
+				t.Fatalf("Images are not equal at (%d, %d)", x, y)
+			}
+		}
+	}
 }
